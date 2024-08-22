2c5868d662798e1d0ba1a5dedcab909c27cdfd65
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2024-08-22 22:54:51 +0530

    feat: init

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 mod pb;
 
-use pb::sf::solana::block_meta::v1::BlockMeta;
+use crate::pb::sol::block::v1::BlockMeta;
 use substreams_database_change::pb::database::DatabaseChanges;
 use substreams_database_change::tables::Tables as DatabaseChangeTables;
-use substreams_solana::pb::sol;
+use substreams_solana::pb::sf::solana::r#type::v1::Block;
 
 #[substreams::handlers::map]
-fn map_block(block: sol::v1::Block) -> Result<BlockMeta, substreams::errors::Error> {
+fn map_block(block: Block) -> Result<BlockMeta, substreams::errors::Error> {
     let mut block_height: Option<u64> = None;
     if let Some(v) = block.block_height.as_ref() {
         block_height = Some(v.block_height)
--- src/pb/mod.rs
+++ src/pb/mod.rs
@@ -1,12 +1,21 @@
 // @generated
 pub mod sf {
     pub mod solana {
-        pub mod block_meta {
-            // @@protoc_insertion_point(attribute:sf.solana.block_meta.v1)
+        pub mod r#type {
+            // @@protoc_insertion_point(attribute:sf.solana.type.v1)
             pub mod v1 {
-                include!("sf.solana.block_meta.v1.rs");
-                // @@protoc_insertion_point(sf.solana.block_meta.v1)
+                include!("sf.solana.type.v1.rs");
+                // @@protoc_insertion_point(sf.solana.type.v1)
             }
+        }
+    }
+}
+pub mod sol {
+    pub mod block {
+        // @@protoc_insertion_point(attribute:sol.block.v1)
+        pub mod v1 {
+            include!("sol.block.v1.rs");
+            // @@protoc_insertion_point(sol.block.v1)
         }
     }
 }
